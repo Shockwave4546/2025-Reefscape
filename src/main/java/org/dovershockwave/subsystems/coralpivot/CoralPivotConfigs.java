aab30eadc0c5c93dbbb971c05463829a2f610445
Lin Pan <linpan0328@gmail.com>
2025-02-15 16:36:48 -0500

    Flip follower and master on coral pivot arm

--- src/main/java/org/dovershockwave/subsystems/coralpivot/CoralPivotConfigs.java
+++ src/main/java/org/dovershockwave/subsystems/coralpivot/CoralPivotConfigs.java
@@ -35,19 +35,19 @@ public class CoralPivotConfigs {
             .busVoltagePeriodMs(20)
             .outputCurrentPeriodMs(20);
 
-    BIGGER_PIVOT_LEFT_CONFIG
+    BIGGER_PIVOT_RIGHT_CONFIG
             .inverted(CoralPivotConstants.BIGGER_PIVOT_INVERTED)
             .idleMode(SparkBaseConfig.IdleMode.kBrake)
             .smartCurrentLimit(Constants.NEO_CURRENT_LIMIT)
             .voltageCompensation(12.0);
-    BIGGER_PIVOT_LEFT_CONFIG.absoluteEncoder
+    BIGGER_PIVOT_RIGHT_CONFIG.absoluteEncoder
             .positionConversionFactor(CoralPivotConstants.BIGGER_PIVOT_POSITION_CONVERSION_FACTOR)
             .velocityConversionFactor(CoralPivotConstants.BIGGER_PIVOT_VELOCITY_CONVERSION_FACTOR)
             .averageDepth(2);
-    BIGGER_PIVOT_LEFT_CONFIG.closedLoop
+    BIGGER_PIVOT_RIGHT_CONFIG.closedLoop
             .feedbackSensor(ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder)
             .pidf(CoralPivotConstants.BIGGER_PIVOT_GAINS.p(), CoralPivotConstants.BIGGER_PIVOT_GAINS.i(), CoralPivotConstants.BIGGER_PIVOT_GAINS.d(), CoralPivotConstants.BIGGER_PIVOT_GAINS.ff());
-    BIGGER_PIVOT_LEFT_CONFIG.signals
+    BIGGER_PIVOT_RIGHT_CONFIG.signals
             .absoluteEncoderPositionAlwaysOn(true)
             .absoluteEncoderPositionPeriodMs((int) (1000.0 / ODOMETRY_FREQUENCY))
             .absoluteEncoderVelocityAlwaysOn(true)
@@ -56,7 +56,7 @@ public class CoralPivotConfigs {
             .busVoltagePeriodMs(20)
             .outputCurrentPeriodMs(20);
 
-    BIGGER_PIVOT_RIGHT_CONFIG
+    BIGGER_PIVOT_LEFT_CONFIG
             .follow(CoralPivotConstants.BIGGER_PIVOT_LEFT_SPARK_ID)
             .idleMode(SparkBaseConfig.IdleMode.kBrake)
             .smartCurrentLimit(Constants.NEO_CURRENT_LIMIT)
