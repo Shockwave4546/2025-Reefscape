bad8e437da4bda81412121e1c7af6f7d0a3a42c2
Lin Pan <linpan0328@gmail.com>
2025-02-15 01:41:22 -0500

    Start adding ff for elevator/arm

--- src/main/java/org/dovershockwave/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/dovershockwave/subsystems/elevator/ElevatorConstants.java
@@ -1,5 +1,6 @@
 package org.dovershockwave.subsystems.elevator;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import org.dovershockwave.utils.ElevatorFeedforwardConstants;
 import org.dovershockwave.utils.PIDFGains;
@@ -13,7 +14,7 @@ public class ElevatorConstants {
   public static final double POSITION_CONVERSION_FACTOR = (2 * Math.PI) / 125.0;
   public static final double VELOCITY_CONVERSION_FACTOR = ((2 * Math.PI) / 125.0) / 60.0;
   public static final PIDFGains PID_GAINS = new PIDFGains(0.0, 0.0, 0.0, 0.0);
-  public static final ElevatorFeedforwardConstants FEEDFORWARD_GAINS = new ElevatorFeedforwardConstants(0.0, 0.0, 0.0, 0.0);
+  public static final ElevatorFeedforwardConstants FEEDFORWARD_GAINS = new ElevatorFeedforwardConstants(0.0, 0.0, 0.0, 0.0, new TrapezoidProfile.Constraints(0.0, 0.0));
   public static final double POSITION_TOLERANCE = Units.degreesToRadians(2.5);
 
   public static final double LIDAR_STARTING_DISTANCE = 0.0; // TODO: 1/30/25  
