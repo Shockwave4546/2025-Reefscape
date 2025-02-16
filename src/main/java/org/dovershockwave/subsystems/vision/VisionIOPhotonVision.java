678505637d4febe5d897c1aa25e96dce9620333b
Lin Pan <linpan0328@gmail.com>
2025-02-16 15:35:19 -0500

    Implement 6328 pnpDistanceTrigSolve

--- src/main/java/org/dovershockwave/subsystems/vision/VisionIOPhotonVision.java
+++ src/main/java/org/dovershockwave/subsystems/vision/VisionIOPhotonVision.java
@@ -1,21 +1,23 @@
 package org.dovershockwave.subsystems.vision;
 
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.*;
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class VisionIOPhotonVision implements VisionIO {
   protected final PhotonCamera camera;
   protected final Transform3d robotToCamera;
+  private final Supplier<Rotation2d> robotHeadingSupplier;
 
-  public VisionIOPhotonVision(CameraType type) {
+  public VisionIOPhotonVision(CameraType type, Supplier<Rotation2d> robotHeadingSupplier) {
     camera = new PhotonCamera(type.name);
     this.robotToCamera = type.robotToCamera;
+    this.robotHeadingSupplier = robotHeadingSupplier;
   }
 
   @Override public void updateInputs(VisionIOInputs inputs) {
@@ -79,43 +81,48 @@ public class VisionIOPhotonVision implements VisionIO {
         if (!result.hasTargets()) return;
         if (!VisionConstants.DO_SINGLE_TAG_POSE_ESTIMATE) return;
         final var bestTarget = result.getBestTarget();
-
-        // Get the target pose relative to the camera
-        final var cameraToTarget = bestTarget.getBestCameraToTarget();
-
-        // Invert robotToCamera to get cameraToRobot
-        final var cameraToRobot = this.robotToCamera.inverse();
-
-        // Combine cameraToTarget with cameraToRobot to find the robot pose relative to the target
-        final var robotToTarget = cameraToTarget.plus(cameraToRobot);
-        // TODO: 1/30/25 This needs to be resolved later bc the optional can be empty.
-        final var fieldToTarget = VisionConstants.APRIL_TAG_FIELD.getTagPose(bestTarget.fiducialId).get(); // Define the field-to-target Pose3d (AprilTag position on the field)
-
-        // Combine fieldToTarget with the inverted robotToTarget to get fieldToRobot
-        final var fieldToRobot = fieldToTarget.transformBy(robotToTarget.inverse());
-
-        tagIds.add((short) bestTarget.fiducialId);
-
-        poseObservations.add(new PoseObservation(
-                result.getTimestampSeconds(), // Timestamp
-                fieldToRobot, // 3D pose estimate
-                bestTarget.poseAmbiguity, // Ambiguity
-                1, // Tag count
-                cameraToTarget.getTranslation().getNorm())); // Average tag distance
+        final var tagDistance = bestTarget.getBestCameraToTarget().getTranslation().getNorm();
+        pnpDistanceTrigSolve(bestTarget).ifPresent(robotPose3d -> {
+          tagIds.add((short) bestTarget.fiducialId);
+          poseObservations.add(new PoseObservation(
+                  result.getTimestampSeconds(), // Timestamp
+                  robotPose3d, // 3D pose estimate
+                  bestTarget.poseAmbiguity, // Ambiguity
+                  1, // Tag count
+                  tagDistance)); // Average tag distance
+                }
+        );
       });
     }
 
     // Save pose observations to inputs object
-    inputs.poseObservations = new PoseObservation[poseObservations.size()];
-    for (int i = 0; i < poseObservations.size(); i++) {
-      inputs.poseObservations[i] = poseObservations.get(i);
-    }
+    inputs.poseObservations = poseObservations.toArray(new PoseObservation[0]);
+     // Save tag IDs to inputs objects
+    inputs.tagIds = tagIds.stream().mapToInt(i -> i).toArray();
+  }
 
-    // Save tag IDs to inputs objects
-    inputs.tagIds = new int[tagIds.size()];
-    int i = 0;
-    for (int id : tagIds) {
-      inputs.tagIds[i++] = id;
-    }
+  /**
+   * Use distance data from the best visible tag to compute a Pose. This runs on the RoboRIO in order
+   * to access the robot's yaw heading.
+   *
+   * <p>6328 Explanation on Chief Delphi<a href="https://www.chiefdelphi.com/t/frc-6328-mechanical-advantage-2025-build-thread/477314/98">...</a></p>
+   */
+  private Optional<Pose3d> pnpDistanceTrigSolve(PhotonTrackedTarget target) {
+    final var camToTagTranslation = new Translation3d(
+            target.getBestCameraToTarget().getTranslation().getNorm(),
+            new Rotation3d(
+                    0,
+                    -Math.toRadians(target.getPitch()),
+                    -Math.toRadians(target.getYaw())))
+            .rotateBy(robotToCamera.getRotation())
+            .toTranslation2d()
+            .rotateBy(robotHeadingSupplier.get());
+
+    return VisionConstants.APRIL_TAG_FIELD.getTagPose(target.getFiducialId()).map(tagPose3d -> {
+      final var fieldToCameraTranslation = tagPose3d.toPose2d().getTranslation().plus(camToTagTranslation.unaryMinus());
+      final var camToRobotTranslation = robotToCamera.getTranslation().toTranslation2d().unaryMinus().rotateBy(robotHeadingSupplier.get());
+      final var robotPose2d = new Pose2d(fieldToCameraTranslation.plus(camToRobotTranslation), robotHeadingSupplier.get());
+      return new Pose3d(robotPose2d);
+    });
   }
 }
