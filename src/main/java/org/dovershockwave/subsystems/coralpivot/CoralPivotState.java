8d624a2cdd2892b8f682f036a98c4d544aff5d66
Lin Pan <linpan0328@gmail.com>
2025-02-18 22:49:25 -0500

    add a todo moving state for coral pivot so we can line up w the reef hopefully

--- src/main/java/org/dovershockwave/subsystems/coralpivot/CoralPivotState.java
+++ src/main/java/org/dovershockwave/subsystems/coralpivot/CoralPivotState.java
@@ -3,6 +3,7 @@ package org.dovershockwave.subsystems.coralpivot;
 // TODO: 2/1/2025 the bigger pivot's angles still need to be set. 
 public record CoralPivotState(double wristPositionRad, double armPositionRad) {
   public static final CoralPivotState STARTING = new CoralPivotState(0, -1.2); // Good
+  public static final CoralPivotState MOVING = new CoralPivotState(0, 0.0); // TODO: 2/18/2025
   public static final CoralPivotState HUMAN_PLAYER = new CoralPivotState(Math.PI / 2.0, 0.0); // TODO: 2/1/2025  
   public static final CoralPivotState L1 = new CoralPivotState(Math.PI / 2.0, -0.4); // TODO: 2/1/2025
   public static final CoralPivotState L2 = new CoralPivotState(0.0, 0.0); // TODO: 2/1/2025  
