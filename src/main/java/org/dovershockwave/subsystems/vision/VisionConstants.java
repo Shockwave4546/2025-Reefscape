b9bfd4ebc755e844cc967d4dc96a1a0979cd0c95
Lin Pan <linpan0328@gmail.com>
2025-03-01 22:37:10 -0500

    put vision constants to the same as the ones in HolonomicDriveController bc they do the same thing

--- src/main/java/org/dovershockwave/subsystems/vision/VisionConstants.java
+++ src/main/java/org/dovershockwave/subsystems/vision/VisionConstants.java
@@ -17,9 +17,9 @@ public class VisionConstants {
   public static final double LINEAR_STD_DEV_BASELINE = 0.02; // Meters
   public static final double ANGULAR_STD_DEV_BASELINE = 0.06; // Radians
 
-  public static final PIDFGains ALIGNMENT_OMEGA_PID = new PIDFGains(15, 0.0, 0, 0.0);
-  public static final PIDFGains ALIGNMENT_X_VELOCITY_PID = new PIDFGains(5, 0.0, 0, 0);
-  public static final PIDFGains ALIGNMENT_Y_VELOCITY_PID = new PIDFGains(5, 0.0, 0, 0);
+  public static final PIDFGains ALIGNMENT_OMEGA_PID = new PIDFGains(2.91, 0.0, 0.094, 0.0);
+  public static final PIDFGains ALIGNMENT_X_VELOCITY_PID = new PIDFGains(10.0, 0.0, 0.08, 0.0);
+  public static final PIDFGains ALIGNMENT_Y_VELOCITY_PID = new PIDFGains(10.0, 0.0, 0.08, 0.0);
 
   public static final double ALIGNMENT_RAD_TOLERANCE = Units.degreesToRadians(3);
   public static final double ALIGNMENT_X_METERS_TOLERANCE = Units.inchesToMeters(2);
