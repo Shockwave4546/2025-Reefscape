84137c78dcf856d1c535a84d7d69c6dfe8e1e2d2
Lin Pan <linpan0328@gmail.com>
2025-03-13 15:23:16 -0400

    3. Fine Tune -> Tune the outtake time, cus rn it's longer than needed

--- src/main/java/org/dovershockwave/commands/FullScoreCoralCommand.java
+++ src/main/java/org/dovershockwave/commands/FullScoreCoralCommand.java
@@ -1,7 +1,9 @@
 package org.dovershockwave.commands;
 
-import edu.wpi.first.wpilibj2.command.*;
-import org.dovershockwave.ReefScoringPosition;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import org.dovershockwave.ReefScoringSelector;
 import org.dovershockwave.subsystems.coralpivot.CoralPivotState;
 import org.dovershockwave.subsystems.coralpivot.CoralPivotSubsystem;
@@ -17,10 +19,11 @@ public class FullScoreCoralCommand extends SequentialCommandGroup {
             new WaitUntilCommand(elevator::atDesiredState),
             new InstantCommand(() -> coralPivot.setDesiredState(selector.getLevel()), coralPivot),
             new WaitUntilCommand(coralPivot::atDesiredState),
-            new ParallelCommandGroup(
-                    new ConditionalCommand(new WaitCommand(0.5).andThen(new InstantCommand(() -> coralPivot.setDesiredState(CoralPivotState.L2_L3_OUTTAKE), coralPivot)), new InstantCommand(), () -> selector.getLevel() == ReefScoringPosition.ReefLevel.L2 || selector.getLevel() == ReefScoringPosition.ReefLevel.L3),
-                    new RunCommand(() -> coralRollers.setDesiredState(selector.getLevel()), coralRollers)
-            ).withTimeout(1),
+//            new ParallelCommandGroup(
+//                    new ConditionalCommand(new WaitCommand(0.5).andThen(new InstantCommand(() -> coralPivot.setDesiredState(CoralPivotState.L2_L3_OUTTAKE), coralPivot)), new InstantCommand(), () -> selector.getLevel() == ReefScoringPosition.ReefLevel.L2 || selector.getLevel() == ReefScoringPosition.ReefLevel.L3),
+//                    new RunCommand(() -> coralRollers.setDesiredState(selector.getLevel()), coralRollers)
+//            ).withTimeout(1),
+            new RunCommand(() -> coralRollers.setDesiredState(selector.getLevel()), coralRollers).withTimeout(0.5),
             new InstantCommand(() -> coralPivot.setDesiredState(CoralPivotState.MOVING), coralPivot),
             new InstantCommand(() -> coralRollers.setDesiredState(CoralRollersState.STOPPED), coralRollers),
             new WaitUntilCommand(coralPivot::atDesiredState),
